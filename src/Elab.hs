fc0b01ccff738598db00e164b44d8ea095b01907
pitinari <tomas.pitinari@gmail.com>
2022-08-31 16:07:32 -0300

    sugar oh honey honey

--- src/Elab.hs
+++ src/Elab.hs
@@ -24,7 +24,7 @@ elab' :: [Name] -> STerm -> Term
 elab' env (SV p v) =
   -- Tenemos que ver si la variable es Global o es un nombre local
   -- En env llevamos la lista de nombres locales.
-  if v `elem` env 
+  if v `elem` env
     then  V p (Free v)
     else V p (Global v)
 
@@ -38,8 +38,11 @@ elab' env (SBinaryOp i o t u) = BinaryOp i o (elab' env t) (elab' env u)
 elab' env (SPrint i str t) = Print i str (elab' env t)
 -- Aplicaciones generales
 elab' env (SApp p h a) = App p (elab' env h) (elab' env a)
-elab' env (SLet p (v,vty) def body) =  
+elab' env (SLet p _ [(v,vty)] def body) =
   Let p v vty (elab' env def) (close v (elab' (v:env) body))
+elab' env (SLet p False ((fv,fty):args) def body) =
+  Let p fv (foldr1 FunTy (map snd args ++ [fty])) (elab' (map fst args ++ env) (SLam p args def)) (close fv $ elab' (fv:env) body)
+elab' env (SLet p True bindings def body) = _ -- PARECIDO AL LAM
 
 elabDecl :: Decl STerm -> Decl Term
 elabDecl = fmap elab
